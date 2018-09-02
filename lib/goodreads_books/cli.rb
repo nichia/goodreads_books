56602bff0d491fcb54372ce5d59183ccc06c00bb
nichia <nichia@gmail.com>
2018-09-02 13:55:58 -0400

    update Cli class to change the screen display and logic

--- lib/goodreads_books/cli.rb
+++ lib/goodreads_books/cli.rb
@@ -4,7 +4,19 @@ class GoodreadsBooks::CLI
     @@choice_awards = nil
   end
 
-  def call(awards_year = nil)
+  def call
+    system "clear"
+    puts ""
+    puts "---------- Welcome to Goodreads Choice Awards Books ----------"
+    puts "           ----------------------------------------"
+    puts ""
+
+    load_choice_awards
+
+    main_menu
+  end #-- call --
+
+  def load_choice_awards(awards_year = nil)
     if awards_year == nil
       puts "Loading The Latest Winners of Goodreads Choice Awards Books..."
     else
@@ -12,10 +24,7 @@ class GoodreadsBooks::CLI
     end
 
     @choice_awards = GoodreadsBooks::Scraper.find_or_create_by_year(awards_year)
-
-    main_menu
-
-  end #-- call --
+  end #-- load_choice_awards --
 
   def main_menu
     system "clear"
@@ -25,33 +34,38 @@ class GoodreadsBooks::CLI
       list_books
 
       puts ""
-      puts "Enter a number to view details of the book, or"
-      puts "select another Choice Awards year (2010 to previous year)."
+      puts "Enter a number to view details of the book, or select another Choice Awards year (2010 onwards)."
       puts "Type 'exit' to end the application."
       input = gets.strip
 
       if input.downcase == "exit"
-        puts ""
-        puts "Thank you for using Goodreads Choice Awards Books."
+        break
       elsif input.to_i.between?(1, GoodreadsBooks::Book.all_by_year(@choice_awards.awards_year).count)
         book = GoodreadsBooks::Book.all_by_year(@choice_awards.awards_year)[input.to_i - 1]
         view_book(book)
+
+        puts ""
+        puts "Press enter to continue or 'exit' to end the application.".colorize(:blue)
+        input = gets.strip.downcase
+
       elsif input.to_i.between?(2010, Time.now.year - 1)
         # This application only works for year 2010 to current year - 1.
         # Goodreads Choice Awards Winner 2009 page setup differs from 2010 onwards.
-        call(input.to_i)
+        load_choice_awards(input.to_i)
       else
         puts ""
-        puts "I don't understand that answer."
+        puts "I don't understand that answer.".colorize(:red)
       end
-
     end
+
+    puts ""
+    puts "Thank you for using Goodreads Choice Awards Books."
   end #-- main_menu --
 
   def list_books
     puts ""
     puts "---------- #{@choice_awards.awards_year} Goodreads Choice Awards Books ----------"
-    puts "           =================================="
+    puts ""
 
     GoodreadsBooks::Book.all_by_year(@choice_awards.awards_year).each.with_index(1) do |book, index|
       puts "#{index}. #{book.title} - #{book.author} - #{book.category}"
@@ -60,7 +74,7 @@ class GoodreadsBooks::CLI
 
   def view_book(book)
     puts ""
-    puts "---------- #{@choice_awards.awards_year} Best #{book.category} ----------".colorize(:blue)
+    puts "---------- #{@choice_awards.awards_year} Best #{book.category} ----------"
     puts "Title:        #{book.title}"
     puts "Author:       #{book.author}"
     puts "Description:  #{book.description}"
